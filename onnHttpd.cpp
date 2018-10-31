1bfcf300c314df1a34c79dc2f5890d0ac28a20c8
t2dk49580 <171671924@qq.com>
2018-11-01 06:42:58 +0800

    remove die exit

--- onnHttpd.cpp
+++ onnHttpd.cpp
@@ -502,8 +502,9 @@ void onnHttpd::runHttpd(int pPort)
     while (1)
     {
         client_sock = accept(server_sock,(struct sockaddr *)&client_name,&client_name_len);
-        if (client_sock == -1)
-            error_die("accept");
+        if (client_sock == -1){
+            continue;
+        }
         /* accept_request(&client_sock); */
         //if (pthread_create(&newthread , NULL, (void *)accept_request, (void *)(intptr_t)client_sock) != 0)
         //    perror("pthread_create");
